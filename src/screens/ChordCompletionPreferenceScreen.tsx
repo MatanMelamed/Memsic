881ad18c2f4a6cd40b475489a2e8f088670d29f3
Matan Melamed <melamed.matan1@gmail.com>
2022-10-07 21:50:49 +0300

    chord completion first 2 settings

--- src/screens/ChordCompletionPreferenceScreen.tsx
+++ src/screens/ChordCompletionPreferenceScreen.tsx
@@ -1,13 +1,88 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, Switch, StyleSheet } from 'react-native'
+import React, { useState } from 'react'
+import Slider from '@react-native-community/slider'
 
 
 const ChordCompletionPreferenceScreen = () => {
+
+    const [newChordOnCompletion, setNewChordOnCompletion] = useState(false)
+
+    const [newChordTimeDelay, setNewChordTimeDelay] = useState(1);
+
+
     return (
-        <View>
-            <Text>ChordCompletionPreferenceScreen</Text>
-        </View>
+        <View style={styles.allSettingsContainer}>
+
+            <View style={styles.settingContainer}>
+                <View style={styles.settingLabelContainer}>
+                    <Text style={styles.settingLabel}>New chord on completion</Text>
+                </View>
+                <View style={styles.settingValueContainer}>
+                    <Switch
+                        onValueChange={() => { setNewChordOnCompletion(!newChordOnCompletion) }}
+                        value={newChordOnCompletion}
+                    />
+                </View>
+            </View>
+
+            <View style={styles.settingContainer}>
+                <View style={styles.settingLabelContainer}>
+                    <Text style={styles.settingLabel}>Time delay for new chord</Text>
+                </View>
+                <View style={styles.settingValueContainer}>
+                    <Slider
+                        disabled={!newChordOnCompletion}
+                        style={{ width: '85%', height: 40 }}
+                        step={1}
+                        minimumValue={1}
+                        maximumValue={10}
+                        value={newChordTimeDelay}
+                        onValueChange={(v) => { setNewChordTimeDelay(v) }}
+                        minimumTrackTintColor="#FFFFFF"
+                        maximumTrackTintColor="#000000"
+                    />
+                    <Text style={{}}>
+                        {newChordTimeDelay}
+                    </Text>
+                </View>
+            </View>
+
+        </View >
     )
 }
+
+
+const styles = StyleSheet.create({
+    allSettingsContainer: {
+
+    },
+
+    settingContainer: {
+        height: 45,
+        flexDirection: 'row',
+        borderWidth: 1
+    },
+    settingLabelContainer: {
+        borderWidth: 1,
+
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+
+    },
+    settingValueContainer: {
+        borderWidth: 1,
+
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+
+    settingLabel: {
+        fontSize: 16,
+        fontWeight: '700'
+    },
+})
 
 export default ChordCompletionPreferenceScreen
