f9ec8a0f52f80df80e6e3f3b273bb25faaaeb915
Matan Melamed <melamed.matan1@gmail.com>
2022-10-10 19:39:38 +0300

    name & icon

--- ios/MemsicTests/MemsicTests.m
+++ ios/MemsicTests/MemsicTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface MusicMemTests : XCTestCase
+@interface MemsicTests : XCTestCase
 
 @end
 
-@implementation MusicMemTests
+@implementation MemsicTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
